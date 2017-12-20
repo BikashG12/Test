8016c556b5cd62891c20be26518001a0e93aa68e
Bikash Gurung <bikash57@live.com>
2017-12-21 00:32:38 +0545

    Added Files

---
+++ MeroTicket/bus_details.php
@@ -0,0 +1,47 @@
+<?php
+	require "connection.php";
+	/*$route_to=$_POST['route_to'];
+	$route_from=$_POST['route_from'];
+*/
+	$bus_date="01-02-2017";
+	$sql="SELECT * FROM bus_list WHERE bus_date ='$bus_date'";
+	
+	
+
+	
+	$res = mysqli_query($con,$sql);
+	$result=array();
+	while($row=mysqli_fetch_array($res)){
+	array_push($result,
+	array('id'=>$row[0],
+		'bus_name'=>$row[1],
+		'bus_seat_no'=>$row[3],
+		'bus_seat_price'=>$row[4],
+		'bus_features'=>$row[5],
+		'bus_time'=>$row[7],
+		'bus_type'=>$row[8]
+		));
+	}
+ 
+	echo json_encode(array("bus_result"=>$result));
+	mysqli_close($con);
+ 
+	/*mysqli_close($con);
+
+	$sql="SELECT * FROM user WHERE Email = '$user_email' AND Password = '$user_password'";
+	$result =mysqli_query($con,$sql);
+	$response=array();
+	if (mysqli_num_rows($result)>0) {
+		$row =mysqli_fetch_row($result);
+		$data_name =$row[1];
+		$message="success";
+		array_push($response,array("code"=>$message,"name"=>$data_name));
+		echo json_encode($response,JSON_UNESCAPED_SLASHES);
+
+	}
+
+*/
+
+
+ 
+?>
---
+++ MeroTicket/connection.php
@@ -0,0 +1,8 @@
+<?php 
+$db_name="localhost";
+$db_username="root";
+$db_password="";
+$db_database_name="meroticket";
+
+$con = mysqli_connect($db_name,$db_username,$db_password,$db_database_name);
+ ?>
